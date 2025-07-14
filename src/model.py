48fcde2287f96474052ebadb21bc54328041ca0c
DaBeans24 <BAIRDS6@my.erau.edu>
2025-07-14 16:11:48 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,11 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+def train_model(X, y):
+    model = LinearRegression()
+    model.fit(X, y)
+    preds = model.predict(X)
+    mse = mean_squared_error(y, preds)
+    print(f"MSE: {mse:.6f}")
+    return model, preds
 
